2ecb63b81cf4f898834c68927d9baca9901a6ca0
Achmad Mufid <achmadmufid.tbn@gmail.com>
2024-02-20 11:47:12 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,58 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// basic routing
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/p', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/nim', function () {
+    return '2241720159<br>Achmad Mufid';
+});
+
+
+// Route Parameters
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/article/{id}', function ($id) {
+    return "Halaman artikel dengan id-" . $id;
+});
+
+
+// Optional Parameters
+
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+
+// route nama
+
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
+
